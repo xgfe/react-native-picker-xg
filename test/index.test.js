e9315f25716d88f339936e260a99e9a8062d102a
lulutia <nowillnowill@gmail.com>
2016-06-02 16:15:41 +0800

    feature(location):add test test

---
+++ test/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Animated,
+  Platform
+} from 'react-native';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+describe('first test', () =>{
+  it("gogo", ()=>{
+    expect(1+2).to.be.equal(3);
+  })
+})
