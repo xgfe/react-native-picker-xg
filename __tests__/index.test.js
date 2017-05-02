4fb37e04bfc52bd9b232a85ee082a6dc5496adce
vivaxy <xyxuye2007@126.com>
2017-05-02 12:59:50 +0800

    test: :white_check_mark:Fix test case

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,15 +3,29 @@ import React from 'react';
 import {
   Modal
 } from 'react-native';
-import {shallow, mount} from 'enzyme';
-import {jsdom} from 'jsdom';
+import { shallow, mount } from 'enzyme';
+import { jsdom } from 'jsdom';
 import Index from '../index.android.js';
-import Picker, {CascadePicker} from '../app/pickerLogic/picker';
+import Picker, { CascadePicker } from '../app/pickerLogic/picker';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+/**
+ * fix missing document
+ * @see http://airbnb.io/enzyme/docs/guides/jsdom.html
+ */
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property];
+  }
+});
 
+global.navigator = {
+  userAgent: 'node.js'
+};
 
 describe('INIT TEST', () => {
   it('check the default value', () => {
@@ -29,7 +43,7 @@ describe('INIT TEST', () => {
         name: '4月',
       },
     }];
-    const wrapper = shallow( <Picker/>);
+    const wrapper = mount(<Picker data={wheel1} />);
 
     expect(wrapper.prop('animationType')).toEqual('none');
     expect(wrapper.prop('visible')).toEqual(false);
